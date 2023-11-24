21d617c2f424ffbeb6974800daebf14c2e8d4dfe
sankar <nsbsankar05@gmail.com>
2023-11-24 15:07:52 +0530

    create and get booking details

---
+++ src/test/java/restfulBooker/CreateBooking.java
@@ -0,0 +1,33 @@
+package restfulBooker;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class CreateBooking {
+    public static void main(String[] args) {
+        //Build request
+        RestAssured.given().log().all()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("booking")
+                .contentType(ContentType.JSON)
+                .body("""
+                        {
+                            "firstname" : "Jim",
+                            "lastname" : "Brown",
+                            "totalprice" : 111,
+                            "depositpaid" : true,
+                            "bookingdates" : {
+                                "checkin" : "2018-01-01",
+                                "checkout" : "2019-01-01"
+                            },
+                            "additionalneeds" : "Breakfast"
+                        }""")
+                //Hit request and get response
+                .post()
+                //Validate response
+                .then().log().all()
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/restfulBooker/GetBooking.java
@@ -0,0 +1,19 @@
+package restfulBooker;
+
+import io.restassured.RestAssured;
+
+public class GetBooking {
+    public static void main(String[] args) {
+
+        RestAssured.given()
+                .log().all()
+                .baseUri("https://restful-booker.herokuapp.com/")
+                .basePath("booking/{id}")
+                .pathParam("id",1)
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200);
+
+    }
+}
